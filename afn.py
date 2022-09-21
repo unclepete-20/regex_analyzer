a329f9b73243e381d38d86b5c971b534d543a7ad
Racso1624 <fer10_lb@outlook.com>
2022-09-20 21:38:20 -0600

    Se agregan funcionalidades basicas de afn

--- afn.py
+++ afn.py
@@ -1,8 +1,12 @@
+from typing import List
+
+
 class AFN(object):
 
     def __init__(self, regex):
         self.regex = regex
         self.regex_postfix = regex.postfixExpresion
+        self.stack_caracteres = list(self.regex_postfix)
         self.contador_estados = 0
         self.estados = []
         self.transiciones = []
@@ -10,24 +14,26 @@ class AFN(object):
         self.estados_aceptacion = []
         self.simbolos = []
         self.estado_inicio = 0
-        self.construccionThompson()
+        self.estado_final = 0
+        self.construccionThompson(self.stack_caracteres)
     
     #ARREGLAR STRING SOLO, ej: a
 
-    def construccionThompson(self):
-        stack_caracteres = []
-        for caracter in self.regex_postfix:
-            print(stack_caracteres)
+    def construccionThompson(self, stack):
+
+        while len(stack) != 0:
+            caracter = stack.pop()
+            
             if(caracter == "."):
                 self.concatenacion()
-            else:
-                stack_caracteres.append(caracter)
+            elif(caracter == "|"):
+                self.union()
+                
 
     def unidad_estados(self, conector):
 
-        if(estado_inicial != 0):
-            self.estados.append(self.contador_estados)
-
+        self.contador_estados += 1
+        self.estados.append(self.contador_estados)
         estado_inicial = self.contador_estados
 
         self.contador_estados += 1
@@ -37,11 +43,56 @@ class AFN(object):
         transicion = [estado_inicial, conector, estado_final]
         self.transiciones.append(transicion)
 
-    def concatenacion(self, exp_1, exp_2):
-        pass
+        return estado_inicial, estado_final
 
-    def union():
-        pass
+    def concatenacion(self):
+        caracter_1 = self.stack_caracteres.pop()
+        caracter_2 = self.stack_caracteres.pop()
+
+        if(caracter_1 in ".|"):
+            pass
+        elif(caracter_2 in ".|"):
+            pass
+        elif(caracter_1 in ".|" and caracter_2 in ".|"):
+            pass
+        else:
+            inicial_1, final_1 = self.unidad_estados(caracter_1)
+            inicial_2, final_2 = self.unidad_estados(caracter_2)
+            transicion = [final_1, "ε", inicial_2]
+            self.transiciones.append(transicion)
+            
+ 
+    def union(self):
+
+        caracter_1 = self.stack_caracteres.pop()
+        caracter_2 = self.stack_caracteres.pop()
+
+        if(caracter_1 in ".|"):
+            pass
+        elif(caracter_2 in ".|"):
+            pass
+        elif(caracter_1 in ".|" and caracter_2 in ".|"):
+            pass
+        else:
+
+            self.contador_estados += 1
+            estado_transicion_1 = self.contador_estados
+
+            inicial_1, final_1 = self.unidad_estados(caracter_1)
+            inicial_2, final_2 = self.unidad_estados(caracter_2)
+
+            self.contador_estados += 1
+            estado_transicion_2 = self.contador_estados
+
+            transicion_1 = [estado_transicion_1, "ε", inicial_1]
+            transicion_2 = [estado_transicion_1, "ε", inicial_2]
+            transicion_3 = [final_1, "ε", estado_transicion_2]
+            transicion_4 = [final_2, "ε", estado_transicion_2]
+
+            self.transiciones.append(transicion_1)
+            self.transiciones.append(transicion_2)
+            self.transiciones.append(transicion_3)
+            self.transiciones.append(transicion_4)
 
     def klenee():
         pass
--- main.py
+++ main.py
@@ -11,5 +11,7 @@ expresion = input("Ingresa la expresion para convertir: ")
 cadena = input("Ingresa la cadena para verficar: ")
 
 regex = Regex(expresion)
+print(regex.postfixExpresion)
+
 afn = AFN(regex)
 print(afn.transiciones)
